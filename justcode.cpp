9474c466cc61408ee19b8a794e08f0105e1f38a1
Unknown <markusdittrich1@gmail.com>
2016-11-16 21:58:51 +0100

    justcode added
    ##Ignore project files unless needed

---
+++ justcode.cpp
@@ -0,0 +1,32 @@
+// VS Projekt oben - funktioniert evtl nur unter windows da system.pause
+
+
+// primefactor_console.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+//Visual Studio includes
+#include "stdafx.h"
+//basic includes
+#include <iostream>
+#include<vector>
+
+int main(void)
+{
+	int nWert = 0;
+	//zählvariablen
+	int i = 0, j = 0;
+	std::cout << "Bitte geben sie einen Integer-Wert ein!" << std::endl;
+	std::cin >> nWert;
+	std::cout << std::endl <<"Wert setzt sich zusammen aus: ";
+	for (i = 2; i <= nWert; i++) {
+		while (nWert%i == 0) {
+			if (j == 0)
+				std::cout << i;
+			else
+				std::cout << " * " << i;
+			j++;
+			nWert /= i;
+		}
+	}
+	std::cout << std::endl;
+	system("pause");
+}
