91cbe7fffce7a0015293d6a51a16092f248161f8
markus <markusdittrich1@gmail.com>
2016-11-18 12:31:08 +0100

    For schleife hinzugefuegt

--- primefactor_console/primefactor_console/primefactor_console.cpp
+++ primefactor_console/primefactor_console/primefactor_console.cpp
@@ -8,20 +8,25 @@
 
 int main(void)
 {
-	int nWert = 0;
+	int nWert = 0, berechnefuerzahl = 0;
 	//zählvariablen
 	int i = 0, j = 0;
 	std::cout << "Bitte geben sie einen Integer-Wert ein!" << std::endl;
 	std::cin >> nWert;	
 	std::cout << std::endl <<"Wert setzt sich zusammen aus: ";
-	for (i = 2; i <= nWert; i++) {
-		while (nWert%i == 0) {
-			if (j == 0) 
-				std::cout << i;
-			else 
-				std::cout << " * " << i;
-			j++;
-			nWert /= i;
+	for (int k = 2; k <= nWert; k++){
+		std::cout << std::endl << "Berechnung fuer " << k << ": ";
+		berechnefuerzahl = k;
+		j = 0;
+		for (i = 2; i <= berechnefuerzahl; i++) {
+			while (berechnefuerzahl%i == 0) {
+				if (j == 0) 
+					std::cout << i;
+				else 
+					std::cout << " * " << i;
+				j++;
+				berechnefuerzahl /= i;
+			}
 		}
 	}
 	std::cout << std::endl;
